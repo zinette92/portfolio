f283bddeb8fbca41251700b44a8be55e40e52a9e
Gasmi Yassine <gasmi.yassine92@gmail.com>
2024-03-09 00:11:15 +0100

    The modale has been added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <section id="about" className="flex flex-col items-center gap-8 py-8">
+      <h2 className="text-4xl font-bold">À propos</h2>
+      <div className="flex w-full  flex-col items-center justify-center gap-4">
+        <p className="text-center">
+          On sait depuis longtemps que travailler avec du texte lisible et
+          contenant du sens est source de distractions, et empêche de se
+          concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum
+          sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il
+          possède une distribution de lettres plus ou moins normale, et en tout
+          cas comparable avec celle du français standard. De nombreuses suites
+          logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem
+          Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum'
+          vous conduira vers de nombreux sites qui n'en sont encore qu'à leur
+          phase de construction. Plusieurs versions sont apparues avec le temps,
+          parfois par accident, souvent intentionnellement (histoire d'y
+          rajouter de petits clins d'oeil, voire des phrases embarassantes).
+        </p>
+        <p className="text-center">
+          On sait depuis longtemps que travailler avec du texte lisible et
+          contenant du sens est source de distractions, et empêche de se
+          concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum
+          sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il
+          possède une distribution de lettres plus ou moins normale, et en tout
+          cas comparable avec celle du français standard. De nombreuses suites
+          logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem
+          Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum'
+          vous conduira vers de nombreux sites qui n'en sont encore qu'à leur
+          phase de construction. Plusieurs versions sont apparues avec le temps,
+          parfois par accident, souvent intentionnellement (histoire d'y
+          rajouter de petits clins d'oeil, voire des phrases embarassantes).
+        </p>
+      </div>
+    </section>
+  );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import Banner from "../../components/Banner/Banner";
 import Projects from "../../components/Projects/Projects";
 import Skills from "../../components/Skills/Skills";
 import Footer from "../../components/Footer/Footer";
+import About from "../../components/About/About";
+import Contact from "../../components/Contact/Contact";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <Banner />
       <Projects />
       <Skills />
+      <About />
+      <Contact />
       <Footer />
     </>
   );
